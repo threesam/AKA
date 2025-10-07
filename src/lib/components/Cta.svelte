0bb0cc1b48906dfd9b721b40d6b18e3edd02f1b9
Sam <57677512+threesam@users.noreply.github.com>
2025-10-06 21:49:23 -0400

    ctas use anton

--- src/lib/components/Cta.svelte
+++ src/lib/components/Cta.svelte
@@ -1,47 +1,47 @@
 <script>
-	import { page } from '$app/stores'
+  import { page } from "$app/stores";
 
-	export let url, text, external, secondary, slug
+  export let url, text, external, secondary, slug;
 </script>
 
 <p
-	class="umami--click--{secondary ? 'secondary-cta' : 'cta'}{$page.url
-		.pathname !== '/'
-		? `-${$page.url.pathname.substring(1).replace(/\//, '-')}`
-		: '-home'}{slug ? `-${slug.current || slug}` : ''}"
+  class="umami--click--{secondary ? 'secondary-cta' : 'cta'}{$page.url
+    .pathname !== '/'
+    ? `-${$page.url.pathname.substring(1).replace(/\//, '-')}`
+    : '-home'}{slug ? `-${slug.current || slug}` : ''}"
 >
-	<a
-		class={secondary ? 'cta secondary' : 'cta'}
-		role="button"
-		target={external ? '_blank' : ''}
-		rel="noopener"
-		href={url}>{text}</a
-	>
+  <a
+    class={secondary ? "cta secondary" : "cta"}
+    role="button"
+    target={external ? "_blank" : ""}
+    rel="noopener"
+    href={url}>{text}</a
+  >
 </p>
 
 <style>
-	a {
-		display: inline-block;
-		text-decoration: none;
-		background: var(--primary);
-		color: var(--background);
-		font-family: var(--headingFont);
-		padding: 5px 13px;
-		border: 0.125rem solid var(--primary);
-		transition: all 0.3s ease-in-out;
-	}
+  a {
+    display: inline-block;
+    text-decoration: none;
+    background: var(--primary);
+    color: var(--background);
+    font-family: "Anton", var(--headingFont);
+    padding: 5px 13px;
+    border: 0.125rem solid var(--primary);
+    transition: all 0.3s ease-in-out;
+  }
 
-	a:hover {
-		transform: scale(0.98);
-	}
+  a:hover {
+    transform: scale(0.98);
+  }
 
-	.secondary {
-		background: none;
-		color: var(--textColor);
-		border: 0.125rem solid transparent;
-	}
+  .secondary {
+    background: none;
+    color: var(--textColor);
+    border: 0.125rem solid transparent;
+  }
 
-	.secondary:hover {
-		color: var(--primary);
-	}
+  .secondary:hover {
+    color: var(--primary);
+  }
 </style>
