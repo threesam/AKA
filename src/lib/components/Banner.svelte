9aa01fd278037a8d2aa3f70c2b85206581548e9b
Sam <57677512+threesam@users.noreply.github.com>
2025-10-10 23:29:54 -0400

    centers banner

--- src/lib/components/Banner.svelte
+++ src/lib/components/Banner.svelte
@@ -2,14 +2,14 @@
   let { category } = $props();
 </script>
 
-<div
-  class="flex flex-col items-end w-full px-5 py-10 text-right mt-20 border-t"
->
+<div class="flex flex-col items-center w-full px-5 py-10 mt-32 text-center">
   <h2 class="font-display text-5xl lg:text-7xl">
     {category?.title || "Category"}
   </h2>
   {#if category?.description}
-    <p class="text-gray-600 dark:text-gray-300 max-w-xl text-lg lg:text-xl">
+    <p
+      class="text-gray-600 dark:text-gray-300 max-w-xl text-lg lg:text-xl text-center"
+    >
       {category.description}
     </p>
   {/if}
