6d04927b2f7ccc03d4f604ec282efce60b1a1710
Sam <57677512+threesam@users.noreply.github.com>
2025-10-23 21:40:28 -0400

    tries not prerendering

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,7 +1,7 @@
 import { client } from "$lib/utils/sanity";
 import { transform } from "$lib/utils/transform";
 
-export const prerender = process.env.NODE_ENV === "production" ? true : false;
+export const prerender = false;
 
 export async function load() {
   try {
